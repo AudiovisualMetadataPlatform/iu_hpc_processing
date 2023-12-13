b8ac85c65b583207f4166aaa4e92a5586a8bcdf8
Brian Wheeler <bdwheele@indiana.edu>
2023-12-13 11:08:40 -0500

    fixes so it should actually work.

--- mdpi_metadata_generator.py
+++ mdpi_metadata_generator.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env hpc_python.sif
 
 import argparse
-from concurrent.futures import Future, ProcessPoolExecutor
+from concurrent.futures import Future, ProcessPoolExecutor, ThreadPoolExecutor
 import json
 import logging
 from pathlib import Path
 import subprocess
 import sys
-import time
+import tempfile
 
 from ffprobe import FFProbe
 from performance import Performance
 import whisper
 import torch
+from mediapipe.tasks.python import audio
+from mediapipe.tasks.python.components import containers
+from scipy.io import wavfile
+import mediapipe as mp
+import numpy as np
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -22,107 +28,201 @@ def main():
     parser.add_argument("--language", default='en', help="Language to use (or 'auto' to detect)")
     parser.add_argument("--device", choices=['auto', 'cuda', 'cpu'], default='auto', help="Device to use")    
     parser.add_argument("--model", default='large', help="Model to use")
-    parser.add_argument("filelist", type=Path, nargs="+", help="File list file for each partition")    
+    parser.add_argument("filelist", type=Path, nargs="+", help="File list file for each partition")
+    parser.add_argument("--perf", type=Path, help="Performance file")   
     args = parser.parse_args()
     logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO,
                         format="%(asctime)s [%(process)d:%(filename)s:%(lineno)d] [%(levelname)s] %(message)s")
 
-    # We're going to assume (rightly?) that our current directory is batch directory
-    # that contains the batch.sh, <jobname>.{out,err}, etc.  This makes things
-    # easier since we can store our performance data here without fear of overwriting
-    # things!
-    batchname = Path('.').absolute().name
-    # Create our performance object
-    bperf = Performance(f"{batchname}.perf", f"Start processing batch {batchname}")        
-    files: list[tuple[Path, Path, FFProbe]] = []    
-    # probe all of the files that we will need to process later.  Save the probe
+    # set up our performance object.
+    perf = Performance(args.perf, f"Processing files in ", autosave=True)
+
+    #  probe all of the files that we will need to process later.  Save the probe
     # data to the disk but keep it in memory too.    
+    files: list[tuple[Path, Path, FFProbe]] = []    
     def probe_done_callback(fut: Future):
         r = fut.result()
         files.append(r)
-        (args.outdir / r[1].name + ".probe").write_text(json.dumps(r[2].probe, indent=2))
+        write_outfile(r[1], args.outdir, "probe", r[2].probe)
         
-    ppe = ProcessPoolExecutor()
-    bperf.checkpoint("Starting FFProbe for all files")
+    ppe = ThreadPoolExecutor()
+    perf.mark('ffprobes')
     for filelist in args.filelist:
-        for file in [Path[x] for x in filelist.read_text().splitlines()]:
+        for file in [Path(x) for x in filelist.read_text().splitlines()]:
             fut = ppe.submit(lambda x: (filelist, x, FFProbe(x)), file)
             fut.add_done_callback(probe_done_callback)    
     ppe.shutdown(wait=True)    
+    perf.checkpoint('ffprobes', len(files))
 
     # fire off all of our processes.
     ppe = ProcessPoolExecutor()
+    perf.mark("processing")
+    def process_done_callback(fut: Future):
+        try:
+            sperf = fut.result()
+            perf.merge(sperf)
+        except Exception as e:
+            logging.exception(f"Exception in process: {e}")
+    
     # there are n*whisper proceseses, one each corresponding to each file list.
     # start them first so they get rolling before the rest of the jobs.
-    for fl in args.filelist:
-        sperf = Performance(fl.name + ".whisper.perf", f"Whisper detection for {fl.name} submitted")
-        ppe.submit(do_whisper, [x for x in files if x[0] == fl], args.outdir, sperf, 
-                   language=args.language, device=args.device)
+    for fl in args.filelist:        
+        fut = ppe.submit(do_whisper, [x for x in files if x[0] == fl], args.outdir,
+                         language=args.language, device=args.device)
+        fut.add_done_callback(process_done_callback)
 
     # for the blank_detection and audio_classification tasks, it's one submission
     # per file.    
-    for file in files:
-        sperf = Performance(f.name + ".blankdetection.perf", f"Blank detection submitted for {f.name}")
-        ppe.submit(do_blankdetection, f, probes[f], args.outdir, sperf, name=f"{f.name} blank detection")
-        sperf = Performance(f.name + ".audioclassification.perf", f"Audio classification submitted for {f.name}")
-        ppe.submit(do_audioclassification, f, args.outdir, sperf,  name=f"{f.name} audio classification")
+    for file in files:        
+        fut = ppe.submit(do_blankdetection, file[1], file[2], args.outdir, )
+        fut.add_done_callback(process_done_callback)
+        fut = ppe.submit(do_audioclassification, file[1], file[2], args.outdir)
+        fut.add_done_callback(process_done_callback)
 
     ppe.shutdown(wait=True)
-    bperf.finish()
+    perf.checkpoint("processing")
+    perf.finish()
 
 
+def write_outfile(srcfile: Path, outdir: Path, key: str, data):
+    """Write the output data in json in a reasonable fashion"""
+    try:
+        with open(outdir / f"{srcfile.name}--{key}.json", "w") as f:
+            json.dump(data, f, indent=2)
+    except Exception as e:
+        logging.exception(f"Cannot write to output file: {srcfile}, {outdir}, {key}, {data}")
 
-def do_whisper(todo: list, outdir: Path, perf: Performance, language='en', device='auto', model='large'):    
-    perf.checkpoint(f"Whisper starting")
+
+def do_whisper(todo: list, outdir: Path, language='en', device='auto', model='large'):    
+    perf = Performance(None)    
     if device == 'auto':
         device = 'cuda' if torch.cuda.is_available() else 'cpu'
     logging.info(f"Using computation device: {device}")
 
     logging.info(f"Loading {model} model")
-    mname = model
-    model = whisper.load_model(model, device=device, download_root="/var/lib/whisper")
-    perf.checkpoint("Loaded Model")
+    perf.mark("whisper-load-model")
+    model_data = whisper.load_model(model, device=device, download_root="/var/lib/whisper")
+    perf.checkpoint("whisper-load-model", model, device)
 
     for audiospec in todo:        
         listfile, audiofile, ffprobe = audiospec
         
-        logging.info(f"Loading audio file {audiofile!s}")
-        perf.checkstart()
-        audio = whisper.load_audio(audiofile)
-        perf.checkpoint(f"Audio load time for {audiofile!s}, duration {ffprobe.get_duration()}")
-
+        afile = str(audiofile.absolute())
+        logging.info(f"{afile}: Loading audio file")
+        perf.mark("whisper-load-audio")
+        audio = whisper.load_audio(afile)
+        perf.checkpoint('whisper-load-audio', afile, ffprobe.get_duration())
+        
         if language == "auto":            
-            logging.info("Detecting language...")  
+            logging.info(f"{afile}: Detecting language...")              
             # Just pull the first few languages that are in tokeniser.py
             probable_languages = ('en', 'zh', 'de', 'es', 'ru', 'ko', 'fr', 'ja')
+            perf.mark('whisper-detect-language')
             detect_audio = whisper.pad_or_trim(audio)
             mel = whisper.log_mel_spectrogram(detect_audio).to(model.device)
             _, probs = model.detect_language(mel)            
             probs = {k: v for k, v in probs.items() if k in probable_languages}
-            logging.info(f"{name}: Language detection: {probs}")
+            logging.info(f"{afile}: Language detection: {probs}")
             language = max(probs, key=probs.get)
-            logging.info(f"{name}: Detected Language: {language}")        
-            prf.checkpoint(f"{name}: detect_language")
-        
-        logging.info(f"{name}: Starting {mname} transcription for {audiofile!s}")
-        res = whisper.transcribe(model, audio, word_timestamps=True, language=language, verbose=None)
-        prf.checkpoint("transcription")
-        write_outfile(audiofile, outdir, name, res)
-        write_outfile(audiofile, outdir, name + "--perf", prf.finish())
-        perf.checkpoint(f"File: {audiofile!s}")
-        logging.info(f"{name}: Transcription {mname} finished for {audiofile!s}")    
+            perf.checkpoint('whisper-detect-language', model, language, afile, ffprobe.get_duration())            
+            
+        logging.info(f"{afile}: Starting {model} transcription")
+        perf.mark('whisper-transcribe')
+        res = whisper.transcribe(model_data, audio, word_timestamps=True, language=language, verbose=None,
+                                 initial_prompt="Hello.")
+        perf.checkpoint('whisper-transcribe', model, afile, ffprobe.get_duration())        
+        write_outfile(audiofile, outdir, f"whisper-{model}", res)                
+        logging.info(f"{afile}: Transcription finished")    
+    
+    return perf
 
 
-def do_blankdetection(file: Path, probe: FFProbe, outdir: Path, perf: Performance):
+def do_blankdetection(file: Path, probe: FFProbe, outdir: Path):
     "Run blank detection on a file"
+    perf = Performance(None)
+    filters = []
+    for stype in probe.get_stream_types():
+        if stype == 'video':
+            filters.append("[0:v]blackdetect=d=5:pix_th=0.10")
+        elif stype == 'audio':
+            filters.append('[0:a]silencedetect=n=-50dB:d=5')
+
+    afile = str(file.absolute())
+    logging.info(f"{afile}: Detecting blank content")
+    perf.mark('blankdetect-ffmpeg')
+    p = subprocess.run(['ffmpeg', '-i', afile, 
+                        '-filter_complex', ";".join(filters),
+                        '-f', 'null', '-'],
+                        stdin=subprocess.DEVNULL, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                        encoding='utf-8')
+    if p.returncode != 0:
+        logging.error(f"{afile}: failed to run ffmpeg {p.returncode}: {p.stdout}")
+        raise Exception(f"Failed to run ffmpeg for blank detection on {afile}")
+    perf.checkpoint('blankdetect-ffmpeg', afile, probe.get_duration())
+    
+    res = []
+    perf.mark('blankdetect-parse')
+    linecount = 0
+    for line in p.stdout.splitlines():
+        linecount += 1
+        if 'silence_end' in line:            
+            parts = line.split()
+            duration = float(parts[7])
+            end = float(parts[4])
+            start = end - duration
+            res.append({'type': 'silence',
+                        'start': start,
+                        'end': end })
+        elif 'black_start' in line:
+            parts = line.split()            
+            start = float(parts[3].split(':')[1])
+            end = float(parts[4].split(':')[1])
+            res.append({'type': 'black',
+                        'start': start,
+                        'end': end})
+    perf.checkpoint('blankdetect-parse', len(res), linecount)
+    write_outfile(file, outdir, 'blankdetect', res)
+    return perf
 
 
-def do_audioclassification(file: Path, outdir: Path, perf: Performance):
+def do_audioclassification(file: Path, probe: FFProbe, outdir: Path):
     "Do audio classification on a file"
+    model_path = "/var/lib/mediapipe/yamnet.tflite"
+    perf = Performance(None)
+    results = []
+    afile = str(file.absolute())    
+    with tempfile.TemporaryDirectory() as tmpdir:
+        perf.mark("audioclassification-cvt2wav")
+        tfile = tmpdir + "/wavefile.wav"
+        logging.info(f"{afile}: Converting to wav")
+        p = subprocess.run(['ffmpeg', '-i', afile, tfile],
+                    stdin=subprocess.DEVNULL, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                    encoding='utf-8')
+        if p.returncode != 0:
+            logging.error(f"{afile}: Cannot run ffmpeg for audio classification")
+            raise Exception("FFMPEG failed for audioclassification on {afile}")
+        perf.checkpoint("audioclassification-cvt2wav", afile, probe.get_duration())
+        
+        BaseOptions = mp.tasks.BaseOptions
+        AudioRunningMode = mp.tasks.audio.RunningMode
+        options = audio.AudioClassifierOptions(
+            base_options=BaseOptions(model_asset_path=model_path),
+            running_mode=AudioRunningMode.AUDIO_CLIPS,
+            max_results=10
+        )
+        logging.info(f"{afile}: Starting classification")
+        perf.mark("audioclassification-classify")
+        with audio.AudioClassifier.create_from_options(options) as classifier:
+            sample_rate, wav_data = wavfile.read(tfile)
+            audio_clip = containers.AudioData.create_from_array(wav_data.astype(float) / np.iinfo(np.int16).max, sample_rate)
+            for c in classifier.classify(audio_clip):
+                results.append({'timestamp_ms': c.timestamp_ms,
+                                'categories': [(y.category_name, y.score) for y in c.classifications[0].categories if y.score >= 0.01]})
+        perf.checkpoint("audioclassification-classify", afile, probe.get_duration())
+        logging.info(f"{afile}: Classification complete")        
+        write_outfile(file, outdir, "audioclassification", results)
 
-
-
-
+    return perf
 
 
 if __name__ == "__main__":
--- performance.py
+++ performance.py
@@ -4,28 +4,95 @@ import logging
 
 
 class Performance:
-    def __init__(self, outfile, title="Performance Object Created"):
-        self.perf = []
-        self.total_start = time.time()
-        self.section_start = time.time()
+    """
+    Create performance statistics for a process.
+
+    The performance data is structed as a dictionary of categories of lists
+    of information:
+
+    {
+        '_script': [<start_time>, <end_time>, <duration>],
+        'cat1': [
+            [<start_time>, <end_time>, <duration>, *additional_context],
+            [<start_time>, <end_time>, <duration>, *additional_context]
+            ....
+        ],
+        'cat2': [
+            [<start_time>, <end_time>, <duration>, *additional_context],
+            [<start_time>, <end_time>, <duration>, *additional_context],
+            ...
+        ],
+        ...
+    }
+    Additional context provides more details for further processing.
+
+    The start time for each category entry is created by calling mark() to
+    start a timer for the given category.
+
+
+    """
+    def __init__(self, outfile, title="Performance Object Created", autosave=False):
+        "Create a performance structure"
+        self.marks = {
+            '_script_start': time.time()
+        }        
+        self.perf = {'_script': [self.marks['_script_start'], None, None, title]}        
         self.outfile = outfile
-        self.perf.append([title, 0])
+        self.autosave = autosave
+        if self.autosave:
+            self.save()
 
-    def checkstart(self):
-        self.section_start = time.time()
 
-    def checkpoint(self, title):
+    def mark(self, tag):
+        "Create (or replace) a timing mark"
+        self.marks[tag] = time.time()
+
+
+    def checkpoint(self, category, *data, mark=None):
+        "Write a category checkpoint"
+        if mark is None:
+            mtime = self.marks.get(category, self.marks['_script_start'])
+        else:
+            mtime = self.marks[mark]
+
         now = time.time()
-        self.perf.append([title, now - self.section_start])
-        self.section_start = now
-        self.save()
+        if category not in self.perf:
+            self.perf[category] = []
+        self.perf[category].append([mtime, now, now - mtime, *data])
+        if self.autosave:
+            self.save()
         return self.perf
+
 
     def finish(self):
-        self.perf.append(['total_run_time', time.time() - self.total_start])
-        self.save()
+        "Mark the script as finished."
+        now = time.time()
+        self.perf['_script'][1] = now
+        self.perf['_script'][2] = now - self.perf['_script'][0]
+        if self.autosave:
+            self.save()
         return self.perf
 
+
     def save(self):
-        with open(self.outfile, "w") as f:
-            json.dump(self.perf, f, indent=2)
+        "Save the performance to a file"
+        if self.outfile is not None:
+            with open(self.outfile, "w") as f:
+                json.dump(self.perf, f, indent=2)
+
+
+    def merge(self, other):
+        """Merge another perf into this one.
+        This only copies the category checkpoints, not the
+        script start, marks, or the filename.
+        """        
+        for cat in other.perf:
+            if cat == '_script':
+                continue
+            if cat not in self.perf:
+                self.perf[cat] = other.perf[cat]
+            else:
+                self.perf[cat].extend(other.perf[cat])
+
+        if self.autosave:
+            self.save()
