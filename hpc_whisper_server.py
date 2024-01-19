40872d6dd181fb4309d9469ba59ccbeb2008454f
Brian Wheeler <bdwheele@indiana.edu>
2024-01-19 09:30:50 -0500

    first attempt at whisper server functionality

--- hpc_whisper_server.py
+++ hpc_whisper_server.py
@@ -9,6 +9,8 @@ import logging
 from concurrent.futures import Future, ProcessPoolExecutor
 import argparse
 from pathlib import Path
+import subprocess
+from utils import write_outfile
 
 def main():
     parser = argparse.ArgumentParser()
@@ -23,7 +25,7 @@ def main():
 
     # determine whisper parameters
     device = data['params'].get('device', 'auto')
-    language = data['params'].get('language', 'en')
+    language = data['params'].get('language', 'auto')
     model = data['params'].get('model', 'medium')
 
     # locate the scp keypair
@@ -41,58 +43,62 @@ def main():
     logging.info("Batches have completed")
 
 
-def do_whisper(todo: list, language='en', device='auto', model='large', scphost=localhost, scpuser=None, keyfile=None):   
+def do_whisper(todo: list, language='auto', device='auto', model='large', scphost='localhost', scpuser=None, keyfile=None):   
     if device == 'auto':
         device = 'cuda' if torch.cuda.is_available() else 'cpu'
     logging.info(f"Using computation device: {device}")
 
     logging.info(f"Loading {model} model")
-    #perf.mark("whisper-load-model")
     model_data = whisper.load_model(model, device=device, download_root="/var/lib/whisper")
-    #perf.checkpoint("whisper-load-model", model, device)
 
-    
-
+    ssh = paramiko.SSHClient()
+    ssh.connect(scphost, username=scpuser, key_filename=keyfile)
+    sftp = ssh.open_sftp()
 
     for spec in todo:
+        try:
+            logging.info(f"Retrieving {spec['infile']}")
+            sftp.get(spec['infile'], "media.mp4")
 
-
-
-
-        listfile, audiofile, ffprobe = audiospec        
-        afile = str(audiofile.absolute())
-        if 'audio' not in ffprobe.get_stream_types():
-            logging.info(f"{afile}: Doesn't contain an audio stream.  Skipping")
-            continue
-
-        logging.info(f"{afile}: Loading audio file")
-        #perf.mark("whisper-load-audio")
-        audio = whisper.load_audio(afile)
-        #perf.checkpoint('whisper-load-audio', afile, ffprobe.get_duration())
+            logging.info(f"Normalizing audio")
+            p = subprocess.run(['ffmpeg', '-i', 'media.mp4', 
+                                '-a:r', '44100', '-a:c', '1', '-c:a', 'pcm_s16le', 
+                                'audio.wav'], stdin=subprocess.DEVNULL, 
+                                stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                                encoding='utf-8')
+            if p.returncode != 0:
+                raise Exception(f"Cannot run ffmpeg on {spec['infile']}: {p.stdout}")
+                
+            audio = whisper.load_audio("audio.wav")
         
-        if language == "auto":            
-            logging.info(f"{afile}: Detecting language...")              
-            # Just pull the first few languages that are in tokeniser.py
-            probable_languages = ('en', 'zh', 'de', 'es', 'ru', 'ko', 'fr', 'ja')
-            #perf.mark('whisper-detect-language')
-            detect_audio = whisper.pad_or_trim(audio)
-            mel = whisper.log_mel_spectrogram(detect_audio).to(model.device)
-            _, probs = model.detect_language(mel)            
-            probs = {k: v for k, v in probs.items() if k in probable_languages}
-            logging.info(f"{afile}: Language detection: {probs}")
-            language = max(probs, key=probs.get)
-            #perf.checkpoint('whisper-detect-language', model, language, afile, ffprobe.get_duration())            
+            if language == "auto":            
+                logging.info(f"{spec['infile']}: Detecting language...")              
+                # Just pull the first few languages that are in tokeniser.py
+                probable_languages = ('en', 'zh', 'de', 'es', 'ru', 'ko', 'fr', 'ja')                
+                detect_audio = whisper.pad_or_trim(audio)
+                mel = whisper.log_mel_spectrogram(detect_audio).to(model.device)
+                _, probs = model.detect_language(mel)            
+                probs = {k: v for k, v in probs.items() if k in probable_languages}
+                logging.info(f"{spec['infile']}: Language detection: {probs}")
+                language = max(probs, key=probs.get)                
             
-        logging.info(f"{afile}: Starting {model} transcription")
-        #perf.mark('whisper-transcribe')
-        res = whisper.transcribe(model_data, audio, word_timestamps=True, language=language, verbose=None,
-                                 initial_prompt="Hello.")
-        #perf.checkpoint('whisper-transcribe', model, afile, ffprobe.get_duration())        
-        write_outfile(audiofile, outdir, f"whisper-{model}", res)                
-        logging.info(f"{afile}: Transcription finished")    
+            logging.info(f"{spec['infile']}: Starting {model} transcription")            
+            res = whisper.transcribe(model_data, audio, word_timestamps=True, language=language, verbose=None,
+                                    initial_prompt="Hello.")            
+            
+            write_outfile(None, "transcript.json", None, res)                
+            logging.info(f"{spec['infile']}: Transcription finished")    
     
-    return perf
+            sftp.put("transcript.json", spec['outfile'])
+            logging.info(f"{spec['outfile']} has been transferred back")
 
+
+        except Exception as e:
+            logging.exception(f"Exception during whisper for {spec['infile']}: {e}")
+
+        finally:
+            for f in ('media.mp4', 'audio.wav', 'transcript.json'):
+                Path(f).unlink(missing_ok=True)
 
 
 
